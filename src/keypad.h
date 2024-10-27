edd42a974a0b55a201e31c41d1defa386993f2d1
Michal Hrbek <michal@dee.cz>
2024-10-27 16:21:26 +0100

    Free memory

--- src/keypad.h
+++ src/keypad.h
@@ -11,6 +11,9 @@ class Keypad {
 		Keypad(uint8_t n_rows, uint8_t n_columns, uint8_t* row_pins, uint8_t* column_pins) : _n_rows(n_rows), _n_columns(n_columns), _row_pins(row_pins), _column_pins(column_pins) {
 			keys = (bool*)malloc(n_rows*n_columns);
 		}
+		~Keypad() {
+			free(keys);
+		}
 		void begin();
 		void poll();
 		uint16_t wait_for_key();
