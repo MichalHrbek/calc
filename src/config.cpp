d6fedee4940abc31f437c51ff493ec8065436a2c
Michal Hrbek <michal@dee.cz>
2024-11-19 22:16:11 +0100

    Fixed linker errors

--- src/config.cpp
+++ src/config.cpp
@@ -1,33 +1,11 @@
 #include "config.h"
 #include "ui.h"
 #include "utils.h"
-#include <SPIFFS.h>
-
-template <typename T>
-void write_config_value(const char* path, T value)
-{
-	File file = SPIFFS.open(path, FILE_WRITE, true);
-	if (file) file.write(&value,sizeof(T));
-}
 
 void write_config_string(const char* path, String value)
 {
 	File file = SPIFFS.open(path, FILE_WRITE, true);
 	if (file) file.write((uint8_t*)value.c_str(),value.length());
-}
-
-template <typename T>
-T read_config_value(const char* path, T default_value)
-{
-	File file = SPIFFS.open(path);
-	if (file)
-	{
-		if (file.size() < sizeof(T)) return default_value;
-		T value;
-		file.readBytes(&value,sizeof(T));
-		return value;
-	}
-	return default_value;
 }
 
 String read_config_string(const char* path, const String default_value)
--- src/config.h
+++ src/config.h
@@ -1,8 +1,30 @@
 #pragma once
 #include <WString.h>
+#include <SPIFFS.h>
 
-template <typename T> T read_config_value(const char* path, T default_value);
-template <typename T> void write_config_value(const char* path, T value);
+// Make sure to keep the byte order correct
+
+template <typename T>
+T read_config_value(const char* path, T default_value)
+{
+	File file = SPIFFS.open(path);
+	if (file)
+	{
+		if (file.size() < sizeof(T)) return default_value;
+		T value;
+		file.readBytes((char*)&value,sizeof(T));
+		return value;
+	}
+	return default_value;
+}
+
+template <typename T>
+void write_config_value(const char* path, T value)
+{
+	File file = SPIFFS.open(path, FILE_WRITE, true);
+	if (file) file.write((uint8_t*)&value,sizeof(T));
+}
+
 String read_config_string(const char* path, const String default_value="");
 void write_config_string(const char* path, String value);
 void config_menu();
